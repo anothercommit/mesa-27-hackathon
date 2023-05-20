3e87e217f6d02020acec1e2fe9cf37fb44688a4a
Your Name <you@example.com>
2023-05-20 11:53:03 -0300

    a

---
+++ app.js
@@ -0,0 +1,10 @@
+const abcMin = "abcdefghijklmnñopqrstuvwxyz";
+const abcMay = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+const numeros = "0123456789";
+const simbolos = "$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+let passLen = 16;
+
+for (let i = 0; i < passLen; i++){
+    
+}
